19f88e92d0d8634c6cbd5514c6802f3f98fa69ab
gentadp12 <arjunadwiputra956@gmail.com>
2026-07-14 00:01:29 +0800

    feat: update api routes and controllers to implement advanced report features and stats

--- routes/api.php
+++ routes/api.php
@@ -4,18 +4,12 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ReportVoteController;
 use App\Http\Controllers\ReportCommentController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
 Route::prefix('auth')->group(function () {
@@ -29,18 +23,23 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Stats endpoint (public for all authenticated)
+    Route::get('/reports/stats', [ReportController::class, 'stats']);
+
     Route::prefix('reports')->group(function () {
         Route::get('/', [ReportController::class, 'index']);
+        Route::get('/{id}', [ReportController::class, 'show']);
         Route::post('/', [ReportController::class, 'store']);
 
         Route::prefix('{reportId}')->group(function () {
             Route::post('/votes', [ReportVoteController::class, 'toggleVote']);
             Route::get('/comments', [ReportCommentController::class, 'index']);
-            Route::post('/comments', [ReportCommentController::class, 'store']);
+            Route::post('/comments', [ReportCommentController::class, 'store'])->middleware('throttle:comments');
         });
 
         Route::middleware('checkRole:admin')->group(function () {
             Route::put('/{id}/status', [ReportController::class, 'updateStatus']);
+            Route::delete('/{id}', [ReportController::class, 'destroy']);
         });
     });
 
@@ -49,5 +48,3 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::delete('/{commentId}', [ReportCommentController::class, 'destroy']);
     });
 });
-
-
